7e4a1c3a6d2a9ddb2a4233850c890aa8db6cd7e6
stars <loinzaft@gmail.com>
2016-08-14 19:04:45 +0900

    added Friendhip mdoel

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813051853) do
+ActiveRecord::Schema.define(version: 20160814095616) do
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "frind_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
